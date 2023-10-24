66fc495e87ec5f3995b81f7d752725503f8ecc71
Ufuk Çelik <ufukcelik.dev@gmail.com>
2023-10-24 22:45:01 +0300

    LibraryManagementSystem

---
+++ LibraryManagementSystem/Controllers/BookController.cs
@@ -0,0 +1,34 @@
+﻿using LibraryManagementSystem.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class BookController : Controller
+    {
+        DbLibEntities db = new DbLibEntities();
+
+        public ActionResult Index()
+        {
+            var book = db.TblBook.ToList();
+            return View(book);
+        }
+
+        [HttpGet]
+        public ActionResult AddBook()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddBook(TblBook p)
+        {
+
+            return View();
+        }
+    }
+}
